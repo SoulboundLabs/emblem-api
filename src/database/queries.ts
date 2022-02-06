9a3f5fd57f533a6e8eb79a885b85f1085e103f95
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-02-06 13:18:45 -0800

    Convert to new query format

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -14,29 +14,16 @@ export const queryLastEarnedBadge = gql`
   }
 `;
 
-export const queryWinnersByProtocol = gql`
-  query WinnerByProtocol($protocolId: String!) {
-    allWinnersList(
-      filter: {
-        earnedBadgesByWinnerId: {
-          some: { protocolId: { equalTo: $protocolId } }
-        }
-      }
-    ) {
-      id
-    }
-  }
-`;
-
-// export const queryWinnersByProtocolBadgeCount = gql`
-//   query WinnerByProtocolByBadgeCount($protocolId: String!) {
-//     allEarnedBadges(filter: { protocolId: { equalTo: $protocolId } }) {
-//       groupedAggregates(groupBy: [WINNER_ID]) {
-//         keys
-//         distinctCount {
-//           id
+// export const queryWinnersByProtocol = gql`
+//   query WinnerByProtocol($protocolId: String!) {
+//     allWinnersList(
+//       filter: {
+//         earnedBadgesByWinnerId: {
+//           some: { protocolId: { equalTo: $protocolId } }
 //         }
 //       }
+//     ) {
+//       id
 //     }
 //   }
 // `;
