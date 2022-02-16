4eee1062ad4a86b0fbd2674a10e27e04840f7671
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2022-02-15 21:23:34 -0700

    No ssl true option Production shouldn't append ssl=true - messes up Render database connection

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      connectionString: process.env.DATABASE_URL + "?ssl=true",
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
     },
     pool: {
